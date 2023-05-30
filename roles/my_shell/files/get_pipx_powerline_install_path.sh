90c2dc101d10840e9876a70b843a60b8fee126e9
CHANGE YOUR NAME!!! <CHANGE_YOUR@EMAIL!!!>
2023-05-30 23:26:13 +0400

    Use pipx instead of pip on new debian versions

---
+++ roles/my_shell/files/get_pipx_powerline_install_path.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pipx_local_venvs=$(pipx environment -v PIPX_LOCAL_VENVS)
+powerline_venvs_root="$pipx_local_venvs/powerline-status"
+root_package_path=$("$powerline_venvs_root"/bin/python -m pip show powerline-status | grep Location | sed -e "s/Location\:\s*\(.*\)/\1/g")
+
+echo "$root_package_path/powerline"
