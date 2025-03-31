c97d7b6b1c747aa29a403093379bef3583e05bd1
CHANGE YOUR NAME!!! <CHANGE_YOUR@EMAIL!!!>
2025-03-31 18:59:43 +0400

    Fix powerline location resolution

--- roles/my_shell/files/get_pipx_powerline_install_path.sh
+++ roles/my_shell/files/get_pipx_powerline_install_path.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-pipx_local_venvs=$(pipx environment -V PIPX_LOCAL_VENVS)
-powerline_venvs_root="$pipx_local_venvs/powerline-status"
-root_package_path=$("$powerline_venvs_root"/bin/python -m pip show powerline-status | grep Location | sed -e "s/Location\:\s*\(.*\)/\1/g")
-
+root_package_path=$(pipx runpip powerline-status show powerline-status | grep Location | sed -e "s/Location\:\s*\(.*\)/\1/g")
 echo "$root_package_path/powerline"
